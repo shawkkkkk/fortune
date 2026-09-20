09178d129eabb157edb72a96bab2bbc3b2c5b05f
shayanel <shayanelhoque@gmail.com>
2026-09-20 09:17:59 -0500

    Add Pancake V3 pool health interface

---
+++ contracts/src/interfaces/IPancakeV3PoolLike.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IPancakeV3PoolLike {
+    function slot0()
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            uint32 feeProtocol,
+            bool unlocked
+        );
+}

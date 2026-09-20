94e5c7ea6bcdd6cd8fc2a6a49e004d8c7844298a
shayanel <shayanelhoque@gmail.com>
2026-09-19 22:54:56 -0500

    Deploy Fortune automation registry on testnet

--- contracts/script/DeployTestnet.s.sol
+++ contracts/script/DeployTestnet.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 
 import {FortuneAssetRegistry} from "../src/FortuneAssetRegistry.sol";
 import {FortuneFactory} from "../src/FortuneFactory.sol";
+import {FortuneAutomationRegistry} from "../src/FortuneAutomationRegistry.sol";
 import {MockGraduationAdapter} from "../src/MockGraduationAdapter.sol";
 import {MockUsdOracle} from "../src/test/MockUsdOracle.sol";
 import {MockQuoteToken} from "../src/test/MockQuoteToken.sol";
@@ -52,8 +53,17 @@ contract DeployTestnet is Script {
         oracle.setPrice(address(mockUsdt), 1e18);
         oracle.setPrice(address(mockWbnb), 600e18);
 
+        FortuneAutomationRegistry automationRegistry =
+            new FortuneAutomationRegistry(deployer);
+
         FortuneFactory factory =
-            new FortuneFactory(deployer, address(registry), deployer);
+            new FortuneFactory(
+                deployer,
+                address(registry),
+                address(automationRegistry),
+                deployer,
+                deployer
+            );
         MockGraduationAdapter graduation = new MockGraduationAdapter();
         factory.setGraduationAdapter(address(graduation));
 
@@ -61,6 +71,7 @@ contract DeployTestnet is Script {
 
         console2.log("FortuneFactory", address(factory));
         console2.log("FortuneAssetRegistry", address(registry));
+        console2.log("FortuneAutomationRegistry", address(automationRegistry));
         console2.log("MockGraduationAdapter", address(graduation));
         console2.log("MockUsdOracle", address(oracle));
         console2.log("Mock tUSDT", address(mockUsdt));

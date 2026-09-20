17d38e237406475c0bf7cffe85454aae5ba6407c
shayanel <shayanelhoque@gmail.com>
2026-09-20 14:07:36 -0500

    Extend fee deployer for tax infrastructure

--- contracts/src/interfaces/IFortuneFeeRouterDeployer.sol
+++ contracts/src/interfaces/IFortuneFeeRouterDeployer.sol
@@ -12,4 +12,23 @@ interface IFortuneFeeRouterDeployer {
         address protocolTreasury,
         uint16[6] calldata feeBps
     ) external returns (address router);
+
+    function deployDividendVault(
+        address factory,
+        address launchToken,
+        address rewardAsset
+    ) external returns (address vault);
+
+    function deployTaxProcessor(
+        address factory,
+        address launchToken,
+        address quoteAsset,
+        address dividendVault,
+        address creator,
+        address liquidityVault,
+        address treasury,
+        address protocolTreasury,
+        address executor,
+        uint16[7] calldata allocationBps
+    ) external returns (address processor);
 }

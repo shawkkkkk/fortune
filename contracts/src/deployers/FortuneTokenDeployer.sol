deba8440e0b8de989e2fe29e20382e3eb4ae49d7
shayanel <shayanelhoque@gmail.com>
2026-09-20 14:07:25 -0500

    Deploy immutable Fortune tax tokens

--- contracts/src/deployers/FortuneTokenDeployer.sol
+++ contracts/src/deployers/FortuneTokenDeployer.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {FortuneToken} from "../FortuneToken.sol";
+import {FortuneTaxToken} from "../FortuneTaxToken.sol";
 import {IFortuneTokenDeployer} from "../interfaces/IFortuneTokenDeployer.sol";
 
 contract FortuneTokenDeployer is IFortuneTokenDeployer {
@@ -46,6 +47,76 @@ contract FortuneTokenDeployer is IFortuneTokenDeployer {
             )
         );
 
+        token = _predict(
+            salt,
+            initCodeHash
+        );
+    }
+
+    function deployTax(
+        TaxTokenParams calldata p
+    ) external returns (address token) {
+        require(
+            msg.sender == p.fortuneFactory,
+            "ONLY_FACTORY"
+        );
+
+        token = address(
+            new FortuneTaxToken{
+                salt: p.salt
+            }(
+                p.fortuneFactory,
+                p.poolConfigurator,
+                p.poolRegistry,
+                p.quoteAsset,
+                p.name,
+                p.symbol,
+                p.supply,
+                p.manifest,
+                p.buyTaxBps,
+                p.sellTaxBps,
+                p.antiFarmerDuration,
+                p.minimumDividendBalance
+            )
+        );
+    }
+
+    function predictTax(
+        TaxTokenParams calldata p
+    ) external view returns (address token) {
+        bytes32 initCodeHash =
+            keccak256(
+                abi.encodePacked(
+                    type(FortuneTaxToken)
+                        .creationCode,
+                    abi.encode(
+                        p.fortuneFactory,
+                        p.poolConfigurator,
+                        p.poolRegistry,
+                        p.quoteAsset,
+                        p.name,
+                        p.symbol,
+                        p.supply,
+                        p.manifest,
+                        p.buyTaxBps,
+                        p.sellTaxBps,
+                        p.antiFarmerDuration,
+                        p.minimumDividendBalance
+                    )
+                )
+            );
+
+        token =
+            _predict(
+                p.salt,
+                initCodeHash
+            );
+    }
+
+    function _predict(
+        bytes32 salt,
+        bytes32 initCodeHash
+    ) internal view returns (address token) {
         token = address(
             uint160(
                 uint256(

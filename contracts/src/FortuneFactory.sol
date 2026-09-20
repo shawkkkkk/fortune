17e04df697b718787d316cc38e9cd029597c69b1
shayanel <shayanelhoque@gmail.com>
2026-09-19 22:44:39 -0500

    Fix OpenZeppelin imports and decimal math

--- contracts/src/FortuneFactory.sol
+++ contracts/src/FortuneFactory.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {FortuneToken} from "./FortuneToken.sol";
 import {FortuneCurve} from "./FortuneCurve.sol";

03d2dc2a17376a8eb3de1b65a048ebe606043f50
shayanel <shayanelhoque@gmail.com>
2026-09-20 14:07:22 -0500

    Extend token deployer for tax launches

--- contracts/src/interfaces/IFortuneTokenDeployer.sol
+++ contracts/src/interfaces/IFortuneTokenDeployer.sol
@@ -2,6 +2,22 @@
 pragma solidity ^0.8.24;
 
 interface IFortuneTokenDeployer {
+    struct TaxTokenParams {
+        address fortuneFactory;
+        address poolConfigurator;
+        address poolRegistry;
+        address quoteAsset;
+        string name;
+        string symbol;
+        uint256 supply;
+        bytes32 manifest;
+        uint16 buyTaxBps;
+        uint16 sellTaxBps;
+        uint32 antiFarmerDuration;
+        uint256 minimumDividendBalance;
+        bytes32 salt;
+    }
+
     function deploy(
         address fortuneFactory,
         string calldata name,
@@ -18,5 +34,13 @@ interface IFortuneTokenDeployer {
         uint256 supply,
         bytes32 manifest,
         bytes32 salt
+    ) external view returns (address token);
+
+    function deployTax(
+        TaxTokenParams calldata params
+    ) external returns (address token);
+
+    function predictTax(
+        TaxTokenParams calldata params
     ) external view returns (address token);
 }

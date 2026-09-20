f38396f36c791b8e3fe5d2713071163e773eeea8
shayanel <shayanelhoque@gmail.com>
2026-09-20 09:05:40 -0500

    Add mock LP position manager

---
+++ contracts/src/test/MockPositionManager.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import {IFortunePositionManager} from "../interfaces/IFortunePositionManager.sol";
+
+contract MockPositionManager is ERC721, IFortunePositionManager {
+    address public lastCollectRecipient;
+    uint256 public lastCollectTokenId;
+    uint256 public nextAmount0;
+    uint256 public nextAmount1;
+
+    constructor() ERC721("Mock LP Position", "MLP") {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+
+    function setCollectAmounts(
+        uint256 amount0,
+        uint256 amount1
+    ) external {
+        nextAmount0 = amount0;
+        nextAmount1 = amount1;
+    }
+
+    function collect(CollectParams calldata params)
+        external
+        payable
+        returns (uint256 amount0, uint256 amount1)
+    {
+        require(
+            ownerOf(params.tokenId) == msg.sender,
+            "NOT_POSITION_OWNER"
+        );
+
+        lastCollectRecipient = params.recipient;
+        lastCollectTokenId = params.tokenId;
+
+        amount0 = nextAmount0;
+        amount1 = nextAmount1;
+    }
+}
